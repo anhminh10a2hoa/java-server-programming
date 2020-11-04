aa35a064ca9b64872ed4de310bbf5e5dab15ac95
Anh Minh <47974381+anhminh10a2hoa@users.noreply.github.com>
2020-11-04 15:30:34 +0200

    assignment7 done

---
+++ assignment7_user/src/servlet/LogoutServlet.java
@@ -0,0 +1,31 @@
+ package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.Principal;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LogoutServlet extends HttpServlet {
+
+public void doGet(HttpServletRequest request, HttpServletResponse response)
+throws ServletException, IOException {
+
+HttpSession session = request.getSession();
+// Here we force the user to logout by invalidating the HttpSession
+	session.invalidate();
+	response.sendRedirect("/assignment7_mainpage/index.html");
+}
+
+public void doPost(HttpServletRequest request, HttpServletResponse response)
+throws ServletException, IOException {
+
+doGet(request, response);
+
+}
+
+}
